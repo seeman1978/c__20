521a65664954300f83740237e97f90e56253fab0
wangqiang <seeman1978@163.com>
2020-02-24 16:37:32 +0800

    c++标准学习

--- Bind.cpp
+++ Bind.cpp
@@ -2,6 +2,12 @@
 // Created by 王强 on 2020/2/24.
 //
 #include <iostream>
+
+struct S {
+    int x1 : 2;
+    volatile double y1;
+};
+
 int main()
 {
     /// 绑定数组
@@ -17,4 +23,13 @@ int main()
     std::cout << "a[0] is " << a[0] << std::endl;
 
     /// 绑定元祖式
+    std::tuple<int, double, std::string> ids;
+    ids = std::make_tuple(1, 2.5, "hello world");
+    const auto& [id, score, name] = ids;
+
+    std::cout << "id is " << id << ". score is " << score << ". name is " << name << "." << std::endl;
+
+    /// 绑定到数据成员
+    const auto [x1, y1] = S();
+    std::cout << "x1 is " << x1 << ". y1 is " << y1 << std::endl;
 }
