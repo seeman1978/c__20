ec74c2fe6f589d1944d53ccc6f705c4f7115ccbb
seeman1978 <seeman1978@163.com>
2020-07-17 11:22:29 +0800

    libpq

--- pg_listen2.cpp
+++ pg_listen2.cpp
@@ -98,7 +98,7 @@ void increase_ragea()
     /*
      * Issue LISTEN command to enable notifications from the rule's NOTIFY.
      */
-    res = PQexec(conn, "LISTEN name_notify");
+    res = PQexec(conn, "LISTEN notify_customcheckinserted");
     if (PQresultStatus(res) != PGRES_COMMAND_OK)
     {
         fprintf(stderr, "LISTEN command failed: %s", PQerrorMessage(conn));
@@ -203,7 +203,7 @@ void increase_rageb()
     /*
      * Issue LISTEN command to enable notifications from the rule's NOTIFY.
      */
-    res = PQexec(conn, "LISTEN name_notify2");
+    res = PQexec(conn, "LISTEN notify_customcheckdeleted");
     if (PQresultStatus(res) != PGRES_COMMAND_OK)
     {
         fprintf(stderr, "LISTEN command failed: %s", PQerrorMessage(conn));
@@ -270,12 +270,222 @@ void increase_rageb()
     fprintf(stderr, "Done.\n");
 }
 
+void increase_ragec()
+{
+    const char *conninfo;
+    conninfo = "host=192.168.137.23 port=5432 dbname=mydb connect_timeout=10 user=postgres password=postgres";
+    PGconn     *conn;
+    PGresult   *res = nullptr;
+    int         nnotifies = 0;
+    PGnotify   *notify = nullptr;
+
+    /* Make a connection to the database */
+    conn = PQconnectdb(conninfo);
+
+    /* Check to see that the backend connection was successfully made */
+    if (PQstatus(conn) != CONNECTION_OK)
+    {
+        fprintf(stderr, "Connection to database failed: %s",
+                PQerrorMessage(conn));
+        exit_nicely(conn);
+    }
+
+    /* Set always-secure search path, so malicious users can't take control. */
+    res = PQexec(conn,
+                 "SELECT pg_catalog.set_config('search_path', '', false)");
+    if (PQresultStatus(res) != PGRES_TUPLES_OK)
+    {
+        fprintf(stderr, "SET failed: %s", PQerrorMessage(conn));
+        PQclear(res);
+        exit_nicely(conn);
+    }
+
+    /*
+     * Should PQclear PGresult whenever it is no longer needed to avoid memory
+     * leaks
+     */
+    PQclear(res);
+
+    /*
+     * Issue LISTEN command to enable notifications from the rule's NOTIFY.
+     */
+    res = PQexec(conn, "LISTEN notify_custommancheckinserted");
+    if (PQresultStatus(res) != PGRES_COMMAND_OK)
+    {
+        fprintf(stderr, "LISTEN command failed: %s", PQerrorMessage(conn));
+        PQclear(res);
+        exit_nicely(conn);
+    }
+    PQclear(res);
+
+    /*
+     * Issue LISTEN command to enable notifications from the rule's NOTIFY.
+     */
+    /*
+    res = PQexec(conn, "LISTEN name_notify2");
+    if (PQresultStatus(res) != PGRES_COMMAND_OK)
+    {
+        fprintf(stderr, "LISTEN command failed: %s", PQerrorMessage(conn));
+        PQclear(res);
+        exit_nicely(conn);
+    }
+    PQclear(res);
+    */
+    /* Quit after four notifies are received. */
+    nnotifies = 0;
+    while (nnotifies < 4)
+    {
+        /*
+         * Sleep until something happens on the connection.  We use select(2)
+         * to wait for input, but you could also use poll() or similar
+         * facilities.
+         */
+        int         sock;
+        fd_set      input_mask;
+
+        sock = PQsocket(conn);
+
+        if (sock < 0)
+            break;              /* shouldn't happen */
+
+        FD_ZERO(&input_mask);
+        FD_SET(sock, &input_mask);
+
+        if (select(sock + 1, &input_mask, NULL, NULL, NULL) < 0)
+        {
+            fprintf(stderr, "select() failed: %s\n", strerror(errno));
+            exit_nicely(conn);
+        }
+
+        /* Now check for input */
+        PQconsumeInput(conn);
+        while ((notify = PQnotifies(conn)) != NULL)
+        {
+            fprintf(stderr,
+                    "ASYNC NOTIFY of '%s' received %s from backend PID %d\n",
+                    notify->relname, notify->extra, notify->be_pid);
+            PQfreemem(notify);
+            nnotifies++;
+            PQconsumeInput(conn);
+        }
+    }
+    /* close the connection to the database and cleanup */
+    PQfinish(conn);
+    fprintf(stderr, "Done.\n");
+}
+
+void increase_raged()
+{
+    const char *conninfo;
+    conninfo = "host=192.168.137.23 port=5432 dbname=mydb connect_timeout=10 user=postgres password=postgres";
+    PGconn     *conn;
+
+    PGresult   *res = nullptr;
+    int         nnotifies = 0;
+    PGnotify   *notify = nullptr;
+
+    /* Make a connection to the database */
+    conn = PQconnectdb(conninfo);
+
+    /* Check to see that the backend connection was successfully made */
+    if (PQstatus(conn) != CONNECTION_OK)
+    {
+        fprintf(stderr, "Connection to database failed: %s",
+                PQerrorMessage(conn));
+        exit_nicely(conn);
+    }
+
+    /* Set always-secure search path, so malicious users can't take control. */
+    res = PQexec(conn,
+                 "SELECT pg_catalog.set_config('search_path', '', false)");
+    if (PQresultStatus(res) != PGRES_TUPLES_OK)
+    {
+        fprintf(stderr, "SET failed: %s", PQerrorMessage(conn));
+        PQclear(res);
+        exit_nicely(conn);
+    }
+
+    /*
+     * Should PQclear PGresult whenever it is no longer needed to avoid memory
+     * leaks
+     */
+    PQclear(res);
+
+    /*
+     * Issue LISTEN command to enable notifications from the rule's NOTIFY.
+     */
+    res = PQexec(conn, "LISTEN notify_custommancheckdeleted");
+    if (PQresultStatus(res) != PGRES_COMMAND_OK)
+    {
+        fprintf(stderr, "LISTEN command failed: %s", PQerrorMessage(conn));
+        PQclear(res);
+        exit_nicely(conn);
+    }
+    PQclear(res);
+
+    /*
+     * Issue LISTEN command to enable notifications from the rule's NOTIFY.
+     */
+    /*
+    res = PQexec(conn, "LISTEN name_notify2");
+    if (PQresultStatus(res) != PGRES_COMMAND_OK)
+    {
+        fprintf(stderr, "LISTEN command failed: %s", PQerrorMessage(conn));
+        PQclear(res);
+        exit_nicely(conn);
+    }
+    PQclear(res);
+    */
+    /* Quit after four notifies are received. */
+    nnotifies = 0;
+    while (nnotifies < 4)
+    {
+        /*
+         * Sleep until something happens on the connection.  We use select(2)
+         * to wait for input, but you could also use poll() or similar
+         * facilities.
+         */
+        int         sock;
+        fd_set      input_mask;
+
+        sock = PQsocket(conn);
+
+        if (sock < 0)
+            break;              /* shouldn't happen */
+
+        FD_ZERO(&input_mask);
+        FD_SET(sock, &input_mask);
+
+        if (select(sock + 1, &input_mask, NULL, NULL, NULL) < 0)
+        {
+            fprintf(stderr, "select() failed: %s\n", strerror(errno));
+            exit_nicely(conn);
+        }
+
+        /* Now check for input */
+        PQconsumeInput(conn);
+        while ((notify = PQnotifies(conn)) != NULL)
+        {
+            fprintf(stderr,
+                    "ASYNC NOTIFY of '%s' received %s from backend PID %d\n",
+                    notify->relname, notify->extra, notify->be_pid);
+            PQfreemem(notify);
+            nnotifies++;
+            PQconsumeInput(conn);
+        }
+    }
+
+    /* close the connection to the database and cleanup */
+    PQfinish(conn);
+
+    fprintf(stderr, "Done.\n");
+}
 
 int
 main(int argc, char **argv)
 {
 
-    std::thread a(increase_ragea), b(increase_rageb);
+    std::thread a(increase_ragea), b(increase_rageb), c(increase_ragec), d(increase_raged);
 
     /*{
         std::lock_guard<std::mutex> lock(cout_mutex);
@@ -284,6 +494,8 @@ main(int argc, char **argv)
 
     a.detach();
     b.detach();
+    c.detach();
+    d.detach();
     std::this_thread::sleep_for(std::chrono::seconds(999999999999));
 
     return 0;
