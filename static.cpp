96227933dc58317bd0a2988ccc0b90493b8572a8
seeman1978 <seeman1978@163.com>
2020-09-04 17:59:04 +0800

    decltype 后置返回类型

---
+++ static.cpp
@@ -0,0 +1,10 @@
+//
+// Created by zhaoyue on 2020/9/4.
+//
+#include <iostream>
+static int i = 9;
+int main()
+{
+    int j  = i * 2;
+    printf("%d", j);
+}
---
+++ string.cpp
@@ -0,0 +1,13 @@
+//
+// Created by zhaoyue on 2020/9/4.
+//
+
+#include <string>
+
+int main()
+{
+    std::wstring s1 = L"hello";
+    std::string s2 = R"(aa)";
+    std::string s3 = "aa";
+    std::string utf8 = u8"你好";
+}
