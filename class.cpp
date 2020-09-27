71d06662d65377659ba755498d912d01b2d8eba0
seeman1978 <seeman1978@163.com>
2020-09-27 15:56:57 +0800

    decltype 后置返回类型

--- class.cpp
+++ class.cpp
@@ -5,9 +5,7 @@
 
 class A{
 public:
-    A(){
-
-    };
+    A()=default;
 
 
     A(int i):m_i(i){
@@ -18,12 +16,17 @@ public:
         std::cout << "d is " << m_d << std::endl;
     }
 
+    ~A()=delete;
 private:
     int m_i;
     double m_d;
 };
 
 int main(){
-    A a = 9;        //调用隐式构造函数
-    A d {7.8};   
+    //A a = 9;        //调用隐式构造函数
+    //A d {7.8};
+    //A a2 = a ;
+
+    A* pA {new A};
+    //delete pA;
 }
