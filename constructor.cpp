bb1e464c181690431c879bc1a75beb242902680b
wangqiang <seeman1978@163.com>
2020-09-27 21:33:30 +0800

    c++标准学习

---
+++ constructor.cpp
@@ -0,0 +1,22 @@
+//
+// Created by 王强 on 2020/9/27.
+//
+
+struct X{
+    int m_i;
+    X()=default;
+    X(int i){
+        m_i = i;
+    }
+};
+
+struct S{
+    int m_j;
+    X m_x;
+
+};
+
+int main(){
+    S s{6};
+    S s2{6, 7}; //逐成员初始化，按照声明的顺序
+}
