e068dbff3879f0138f4c84e0e83e1b049a5edc40
wangqiang <seeman1978@163.com>
2020-10-06 19:54:43 +0800

    c++标准学习 类static成员 未使用，可以不初始化

--- static.cpp
+++ static.cpp
@@ -3,8 +3,19 @@
 //
 #include <iostream>
 static int i = 9;
+
+class A{
+public:
+    static double m_d;  //被使用，必须初始化
+    static int m_i;     //未使用，可以不初始化
+};
+
+double A::m_d = 0.0;
+
 int main()
 {
     int j  = i * 2;
-    printf("%d", j);
+    printf("%d\n", j);
+
+    std::cout << A::m_d << std::endl;
 }
