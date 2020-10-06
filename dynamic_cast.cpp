8f072145e0606e4b55393712d55d27c15cef5c20
wangqiang <seeman1978@163.com>
2020-10-06 09:29:44 +0800

    c++标准学习 final 类不能被继承

--- dynamic_cast.cpp
+++ dynamic_cast.cpp
@@ -1,12 +1,25 @@
 //
 // Created by 王强 on 2020/10/4.
 //
+#include <iostream>
 
 class A{
 
 };
 
 class B{
+
+};
+
+class Base{
+
+};
+
+class Derived: public Base{
+
+};
+
+class Derived2: public Base{
 
 };
 
@@ -20,4 +33,17 @@ int main(){
     B* pB = new B;
 
     //pA = dynamic_cast<A*>(pB);  //无法编译，编译器知道类型不匹配
+
+    Derived* pD = new Derived;
+    Base* pBase = dynamic_cast<Base*>(pD);
+
+    //pD = dynamic_cast<Derived*>(pBase);   //无法编译，编译器知道类型不匹配
+
+    std::cout << typeid(*pBase).name() << std::endl;
+
+    Derived2* pD2 = new Derived2;
+    //pD2 = dynamic_cast<Derived2*>(pBase); //无法编译，编译器知道类型不匹配
+
+
+
 }
