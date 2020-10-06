f0b89b3aa1d99267470dd922b842e38f397b284f
seeman1978 <seeman1978@163.com>
2020-10-06 10:37:02 +0800

    多重继承

--- dynamic_cast.cpp
+++ dynamic_cast.cpp
@@ -10,9 +10,10 @@ class A{
 class B{
 
 };
-
+//dynamic_cast的基类至少包含一个虚函数
 class Base{
-
+public:
+    virtual ~Base(){}
 };
 
 class Derived: public Base{
@@ -37,13 +38,16 @@ int main(){
     Derived* pD = new Derived;
     Base* pBase = dynamic_cast<Base*>(pD);
 
-    //pD = dynamic_cast<Derived*>(pBase);   //无法编译，编译器知道类型不匹配
+    pD = dynamic_cast<Derived*>(pBase);
 
     std::cout << typeid(*pBase).name() << std::endl;
 
     Derived2* pD2 = new Derived2;
-    //pD2 = dynamic_cast<Derived2*>(pBase); //无法编译，编译器知道类型不匹配
-
-
+    pD2 = dynamic_cast<Derived2*>(pBase);
+    if (pD2 == nullptr){
+        std::cout << "pBase is not a pointer to Derived2" << std::endl;
+    } else{
+        std::cout << "pBase is a pointer to Derived2" << std::endl;
+    }
 
 }
