6f6275802e48a70c9e80d88de51f0e2eea4b820a
seeman1978 <seeman1978@163.com>
2020-10-09 08:35:48 +0800

    ？：表达式不允许运算对象不相容

---
+++ condition.cpp
@@ -0,0 +1,12 @@
+//
+// Created by zhaoyue on 2020/10/9.
+//
+
+#include <iostream>
+
+int main(){
+    int i = 0;
+    int a = 9, b = 7;
+    //a>b ? "hello" : b;    string 和int不兼容
+    std::cout << i;
+}
