cf0416fb5c559e5333ba91a6dbca1dab85c7a7bd
wangqiang <seeman1978@163.com>
2020-10-14 23:12:24 +0800

    Matrix

--- Matrix/Matrix.h
+++ Matrix/Matrix.h
@@ -77,9 +77,94 @@ public:
         return elems.data();
     }
 
+    //下标和切片
+    template<typename... Args>
+    Enable_if<Matrix_impl::Requesting_element<Args...>(), T&>
+            operator()(Args... args);
+
+    template<typename... Args>
+    Enable_if<Matrix_impl::Requesting_element<Args...>(), const T&>
+            operator()(Args... args) const;
+
+    template<typename... Args>
+    Enable_if<Matrix_impl::Requesting_slice<Args...>(), Matrix_ref<T,N>>
+    operator()(consg Args&... args);
+
+    template<typename... Args>
+    Enable_if<Matrix_impl::Requesting_slice<Args...>(), Matrix_ref<const T,N>>
+    operator()(const Args&... args) const;
+
+    Matrix_ref<T, N-1> operator[](size_t i){
+        return row(i);
+    }
+
+    Matrix_ref<const T, N-1> operator[](size_t i) const{
+        return row(i);
+    }
+
+    Matrix_ref<T, N-1> row(size_t n);
+    Matrix_ref<const T,N-1> row(size_t n) const;
+
+    Matrix_ref<T,N-1> col(size_t n);
+    Matrix_ref<const T, N-1> col(size_t n) const;
+
+    //算数运算
+    template<typename F>
+    Matrix& apply(F f){
+        for (auto& x: elems){
+            f(x);
+        }
+        return *this;   //返回*this使得链式语法成为可能
+    };
+
+    template<typename M, typename F>
+    Matrix& apply(const M& m, F f){
+        assert(same_extents(desc, m.descriptor()));
+        for (auto i=begin(), j=m.begin(); i!=end(); ++i, ++j){
+            f(*i, *j);
+        }
+        return *this;
+    };
+
+    Matrix& operator=(const T& value);
+
+    Matrix& operator+=(const T& value){
+        return apply([&](T& a){ a+=value;});
+    };
+    Matrix& operator-=(const T& value);
+    Matrix& operator*=(const T& value);
+    Matrix& operator/=(const T& value);
+    Matrix& operator%=(const T& value);
+
+    template<typename M>
+    Enable_if<Matrix_type<M>(), Matrix<T,N>&>
+    operator+=(const M& m){
+        static_assert(m.order()==N, "+=: mismatched Matrix dimensions");
+        assert(same_extents(desc, m.descriptor()));
+        return apply(m, [](T& a, Value_type<M>&b){a+b;});
+    };
+
+    template<typename M>
+    Matrix& operator-=(const M& x);
+
+
 private:
     Matrix_slice<N> desc;   //定义N个维度大小的切片
     std::vector<T> elems;   //元素
 };
+
+template<typename T, size_t N>
+Matrix<T, N> operator+(const Matrix<T,N>& m, const T& value){
+    Matrix<T, N> res = m;
+    res += value;
+    return res;
+}
+
+template<typename T, size_t N>
+Matrix<T, N> operator+(const Matrix<T,N>& a, const Matrix<T,N>& b){
+    Matrix<T, N> res = a;
+    res += b;
+    return res;
+}
 
 #endif //C__20_MATRIX_H
