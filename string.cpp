2b97c6b1a9ac2b9bb8ebac15f3564017f91323ea
seeman1978 <seeman1978@163.com>
2020-10-15 15:10:31 +0800

    c++20 Matrix

--- string.cpp
+++ string.cpp
@@ -5,16 +5,34 @@
 #include <string>
 #include <iostream>
 
+template<typename T>
+void print(const T& var){
+    for (const auto& a : var){
+        std::cout << a << " ";
+    }
+    std::cout << std::endl;
+}
+
 int main()
 {
     std::wstring s1 = L"hello";
     std::string s2 = R"(aa)";
     std::string s3 = "aa";
 
-    std::string utf8 = u8"你好";
-    std::string utf16 = u"你好";
-    std::string utf32 = U"你好";
+    const char8_t *utf8 = u8"你好";
+    const char16_t *utf16 = u"你好";
+    const char32_t *utf32 = U"你好";
+
+    std::u8string str8 = u8"你好";
+    std::u16string str16 = u"你好";
+    std::u32string str32 = U"你好";
 
     setlocale(LC_ALL, ".utf8");
-    std::cout << utf8 << std::endl;
+    print(str8);
+    print(str16);
+    print(str32);
+    //std::cout << str8 << std::endl;
+
+    //std::cout << str16 << std::endl;
+    //std::cout << str32 << std::endl;
 }
