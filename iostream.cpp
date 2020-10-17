0ac8925fbf719c46497592f97282ff33ce3690b7
seeman1978 <seeman1978@163.com>
2020-10-17 16:53:03 +0800

    ostringstream C++20

--- iostream.cpp
+++ iostream.cpp
@@ -2,7 +2,8 @@
 // Created by wq on 2020/10/17.
 //
 #include <iostream>
-#include <sstream>
+#include <iomanip>
+#include <cmath>
 
 int main(){
     std::ostringstream oss{"Label:", std::ios::ate};    //在部追加
@@ -16,7 +17,11 @@ int main(){
     oss2 << "val2";
     std::cout << oss2.str() << std::endl;   //输出val2l2:
 
-    std:stringstream oss3{"", std::ios::ate};
+    std::ostringstream oss3{"", std::ios::ate};
     oss3 << 3 << " hello " << "world";
     std::cout << oss3.str() << std::endl;
+
+    std::cout << std::setiosflags(std::ios::fixed);  //保证setprecision()是设置小数点后的位数。
+    const long double pi = std::acos(-1.L);
+    std::cout << std::setprecision(20) << pi << std::endl;
 }
