594454896151ade8394b71e7bd06a66b65521170
seeman1978 <seeman1978@163.com>
2020-10-24 19:54:17 +0800

    ostringstream C++20

---
+++ algorithm/rotate.cpp
@@ -0,0 +1,16 @@
+//
+// Created by wq on 2020/1;;l;lll/24.
+//
+
+#include <iostream>
+
+int main(){
+    int x =1;
+    int y =3;
+    std::cout << x << std::endl;
+    int z=x/y;
+    std::cout << z << std::endl;
+
+    double d= 1.0/3.0;
+    std::cout << d << std::endl;
+}
