0ee2aa99092830c241d0e66ca41a087df8807feb
seeman1978 <seeman1978@163.com>
2020-10-25 18:52:40 +0800

    ostringstream C++20

---
+++ algorithm/fill.cpp
@@ -0,0 +1,29 @@
+//
+// Created by wq on 2020/10/25.
+//
+
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <iterator>
+
+int main()
+{
+    using namespace std;
+    {
+        std::vector<int> v{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        std::fill(v.begin(), v.end(), -1);
+
+        for (auto elem : v) {
+            std::cout << elem << " ";
+        }
+        std::cout << "\n";
+    }
+    {
+        vector<int> v{0,1,2,3,4,5,6,7,8,9};
+        fill_n(v.begin()+8, 4, -1);
+        copy(v.begin(), v.end(), ostream_iterator<int>(cout, " "));
+        cout << '\n';
+    }
+}
