7b3a3a390bd1c50cdb7650c947ab19863e32a331
seeman1978 <seeman1978@163.com>
2020-10-26 11:39:45 +0800

    ostringstream C++20

--- algorithm/generate.cpp
+++ algorithm/generate.cpp
@@ -33,6 +33,6 @@ int main(){
         std::mt19937 rng; // default constructed, seeded with fixed seed
         std::generate_n(std::ostream_iterator<std::mt19937::result_type>(std::cout, " "),
                         5, std::ref(rng));
-        std::cout << '\n'
+        std::cout << '\n';
     }
 }
