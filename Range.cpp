0f428f48849c7460a7868ee24f5e0faace4d2d62
seeman1978 <seeman1978@163.com>
2020-10-31 19:07:12 +0800

    steady_clock C++20

--- Range.cpp
+++ Range.cpp
@@ -8,13 +8,13 @@
 
 int main(){
     std::vector<int> ints{0,1,2,3,4,5};
-    auto even = [](auto i){
+    auto even = [](auto i) {
         return 0==i%2;
     };
     auto square = [](auto i){
         return i*i;
     };
-    for (auto i : ints | std::views::filter(even) | std::views::transform(square)){
+    for (const auto i : ints | std::views::filter(even) | std::views::transform(square)){
         std::cout << i << "----------------\n";
     }
 }
--- lambda.cpp
+++ lambda.cpp
@@ -11,9 +11,14 @@ int main()
         double b = glambda(3, 3.14); // ok
         std::cout << b << std::endl;
     }
-    {
+    {// c++ 20 feature
         auto glambda = []<typename T>(const T a, const T b) { T c = a + b; T d = c*c; return d; };
         double b = glambda(3, 5); // ok, 3.14 will be failed here.
-        std::cout << b;
+        std::cout << b << std::endl;
+    }
+    {// c++ 20 feature
+        auto glambda = []<typename T>(const T a, const T b) { T c = a + b; T d = c*c; return d; };
+        double b = glambda(3.0, 3.14); // ok
+        std::cout << b << std::endl;
     }
 }
