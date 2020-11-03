836301e5fc6d998c6c9bbc6c7a32a3f0de5dbba6
seeman1978 <seeman1978@163.com>
2020-11-03 17:28:57 +0800

    c++20 range view

---
+++ regex/regex_replace.cpp
@@ -0,0 +1,16 @@
+//
+// Created by zhaoyue on 2020/11/3.
+//
+
+#include <string>
+#include <regex>
+#include <iostream>
+
+int main(){
+    std::string text = "Quick brown fox";
+    std::regex vowel_re{"a|e|i|o|u"};
+    // write the results to an output iterator
+    std::regex_replace(std::ostreambuf_iterator<char>(std::cout), text.begin(), text.end(), vowel_re, "*");
+    // construct a string holding the results
+    std::cout << '\n' << std::regex_replace(text, vowel_re, "[$&]") << '\n';
+}
