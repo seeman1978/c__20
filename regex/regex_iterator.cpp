d8c044bbe48d4a25488146af64a348bd4b3537c9
seeman1978 <seeman1978@163.com>
2020-11-03 17:40:18 +0800

    c++20 range view

---
+++ regex/regex_iterator.cpp
@@ -0,0 +1,23 @@
+//
+// Created by zhaoyue on 2020/11/3.
+//
+
+#include <string>
+#include <regex>
+#include <iostream>
+
+int main(){
+    const std::string s = "Quick brown  fox.";
+    std::regex words_regex{"[^\\s]+"};
+    auto words_begin = std::sregex_iterator(s.begin(), s.end(), words_regex);
+    auto words_end = std::sregex_iterator();
+    std::cout << "Found "
+            << std::distance(words_begin, words_end)
+            << " words:\n";
+
+    for (std::sregex_iterator i = words_begin; i!=words_end; ++i){
+        std::smatch match = *i;
+        std::cout << match.str() << '\n';
+    }
+}
+
