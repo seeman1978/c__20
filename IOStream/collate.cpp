4ae533ff902cd35599a7cbb63a4d39b901847f45
seeman1978 <seeman1978@163.com>
2020-11-06 15:53:23 +0800

    c++20 range view

--- IOStream/collate.cpp
+++ IOStream/collate.cpp
@@ -10,16 +10,16 @@
 
 int main()
 {
-    std::setlocale(LC_ALL, "sv_SE.UTF-8");      //// 用这句 GCC 和 VC编译器都支持输出宽字节
-    //std::wcout.imbue(std::locale("sv_SE.UTF-8"));  //// 为何这句话不起作用 in GCC? 为何这句只支持VC
-    std::wcout << L"LC_ALL: " << std::setlocale(LC_ALL, nullptr) << std::endl;
-    std::wcout << L"LC_CTYPE: " << std::setlocale(LC_CTYPE, nullptr) << std::endl;
+    setlocale(LC_ALL, "sv_SE.UTF-8");      //// 用这句 GCC 和 VC编译器都支持输出宽字节
+    std::wcout.imbue(std::locale("sv_SE.UTF-8"));  //// 为何这句话不起作用 in GCC? 为何这句只支持VC
+    std::wcout << L"LC_ALL: " << setlocale(LC_ALL, nullptr) << std::endl;
+    std::wcout << L"LC_CTYPE: " << setlocale(LC_CTYPE, nullptr) << std::endl;
 
     std::vector<std::wstring> v = {L"ar", L"zebra", L"\u00f6grupp", L"Zebra", L"\u00e4ngel",
                                    L"\u00e5r", L"f\u00f6rnamn"};
 
     std::wcout << "Swedish locale collation order: ";
-    std::sort(v.begin(), v.end(), std::locale("sv_SE.UTF-8"));
+    std::sort(v.begin(), v.end(), std::locale("sv_SE.UTF-8"));  
     for (const auto& s : v) {
         std::wcout << s << ' ';
     }
