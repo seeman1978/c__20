b3e745cc52de34c7417f8da79faa43235b9b2630
seeman1978 <seeman1978@163.com>
2020-11-06 16:45:51 +0800

    steady_clock C++20

--- locale/classic.cpp
+++ locale/classic.cpp
@@ -32,7 +32,7 @@ int main(){
         setlocale(LC_ALL, "sv_SE.UTF-8");      //// 用这句 GCC 和 VC编译器都支持输出宽字节,macos下不起作用
         std::wcout.imbue(std::locale("sv_SE.UTF-8"));  //// 为何这句话不起作用 in GCC? 为何这句只支持VC,clang(macos)
         wcout << L"中文\n";
-        //setlocale(LC_ALL, "sv_SE.UTF-8");      //// 用这句 GCC 和 VC编译器都支持输出宽字节
+
         vector<std::wstring> v = {L"ar", L"zebra", L"\u00f6grupp", L"Zebra", L"\u00e4ngel",
                                   L"\u00e5r", L"f\u00f6rnamn"};
 
