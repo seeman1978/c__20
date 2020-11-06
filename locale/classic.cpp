d7176f10518cb0eeef11509f3c9b8f582e2f143c
seeman1978 <seeman1978@163.com>
2020-11-06 14:37:40 +0800

    steady_clock C++20

--- locale/classic.cpp
+++ locale/classic.cpp
@@ -4,18 +4,25 @@
 
 #include <locale>
 #include <iostream>
+#include <vector>
+
+//do not mix std::cout and std::wcout.
+//first you need to install Swedish on region and language on OS
 
 int main(){
     using namespace std;
     {
-        std::cout << "中文\n";
-        std::wcout << L"中文\n";
-        locale loc1{"zh_CN"};
+        locale loc1{"zh_CN.UTF8"};
         locale loc2 {locale::global(loc1)}; //loc2为旧的全局locale；把全局locale设置为loc1。
         cout << "The name of the previous locale is: " << loc2.name( )
              << "." << endl;
         cout << "The name of the current locale is: " << loc1.name( )
              << "." << endl;
-        std::wcout << L"中文\n";
+        std::cout << "中文\n";
+
+        std::setlocale(LC_ALL, "sv_SE.UTF-8");      //// 用这句 GCC 和 VC编译器都支持输出宽字节
+        std::vector<std::wstring> v = {L"ar", L"zebra", L"\u00f6grupp", L"Zebra", L"\u00e4ngel",
+                                       L"\u00e5r", L"f\u00f6rnamn"};
+
     }
 }
