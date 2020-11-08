912832b159090bdb907b37c16ce75d740d409873
seeman1978 <seeman1978@163.com>
2020-11-08 16:52:38 +0800

    steady_clock C++20

---
+++ locale/get_time.cpp
@@ -0,0 +1,20 @@
+//
+// Created by wq on 2020/11/8.
+//
+#include <iostream>
+#include <locale>
+#include <sstream>
+#include <iomanip>
+
+int main(){
+    std::tm t{};
+    std::istringstream ss("2011-Februar-18 23:12:34");
+    ss.imbue(std::locale("de_DE.utf-8"));
+    ss >> std::get_time(&t, "%Y-%b-%d %H:%M:%S");
+    if (ss.fail()){
+        std::cout << "Parse failed\n";
+    }
+    else{
+        std::cout << std::put_time(&t, "%c") << '\n';
+    }
+}
