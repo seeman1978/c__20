b73bdb9c6f18d9f5b1284251a903926e91b08523
wangqiang <seeman1978@163.com>
2020-11-10 23:53:03 +0800

    c++标准学习 emplace

---
+++ Parallel/is_lock_free.cpp
@@ -0,0 +1,24 @@
+//
+// Created by 王强 on 2020/11/10.
+//
+
+#include <array>
+#include <iostream>
+
+//X上的操作有没有锁，没锁，返回true；有锁，返回false
+struct A{
+    std::array<int, 100> a; //a[100]
+
+};
+
+struct B{
+    int x, y;
+};
+
+int main(){
+    std::cout << std::boolalpha
+        << "std::atomic<A> is lokk free? "
+        << std::atomic<A>{}.is_lock_free() << '\n'
+        << "std::atomic<B> is lock free? "
+        << std::atomic<B>{}.is_lock_free() << '\n';
+}
