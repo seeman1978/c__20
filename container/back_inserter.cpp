3808dd04fe018afa78a233adf6960738f285ea53
wangqiang <seeman1978@163.com>
2020-11-19 21:45:22 +0800

    c++标准学习 利用cv实现生产者和消费者

--- container/back_inserter.cpp
+++ container/back_inserter.cpp
@@ -1,39 +1,31 @@
 //
-// Created by zhaoyue on 2020/11/19.
+// Created by 王强 on 2020/11/17.
 //
 
+#include <list>
 #include <vector>
+#include <algorithm>
+#include <deque>
+#include <set>
 #include <iostream>
 
+template <typename T>
+void printT(const T& t){
+    for (auto i : t){
+        std::cout << i << ' ';
+    }
+    std::cout << '\n';
+}
+
 int main(){
-    /*{
-        std::vector<int> v1{1,2,3};
-        std::vector<int> v2; //EXC_BAD_ACCESS
-        std::copy(v1.begin(), v1.end(), v2.begin());
-        for (auto var:v2){
-            std::cout << var << ' ';
-        }
-        std::cout << '\n';
-    }*/
-
-    {
-        std::vector<int> v1{1,2,3};
-        std::vector<int> v2(3);
-        std::copy(v1.begin(), v1.end(), v2.begin());
-        for (auto var:v2){
-            std::cout << var << ' ';
-        }
-        std::cout << '\n';
-    }
-
-    {
-        std::vector<int> v1{1,2,3};
-        std::vector<int> v2;
-        std::copy(v1.begin(), v1.end(), std::back_inserter(v2));
-        for (auto var:v2){
-            std::cout << var << ' ';
-        }
-        std::cout << '\n';
-    }
-
+    std::list<int> coll1{1,2,3,4,5,6,7,8,9};
+    std::vector<int> coll2;
+    std::copy(coll1.begin(), coll1.end(), std::back_inserter(coll2));
+    printT(coll2);
+    std::deque<int> coll3;
+    std::copy(coll1.begin(), coll1.end(), std::front_inserter(coll3));
+    printT(coll3);
+    std::set<int> coll4;
+    std::copy(coll1.begin(), coll1.end(), std::inserter(coll4, coll4.begin()));
+    printT(coll4);
 }
