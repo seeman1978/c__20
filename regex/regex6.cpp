8c5ad99c0e099a5bbf991fa4b383037af4dc7bc9
wq <seeman1978@163.com>
2021-01-04 22:38:06 +0800

    c++ 20 regex_search smatch

---
+++ regex/regex6.cpp
@@ -0,0 +1,32 @@
+//
+// Created by wq on 2021/1/4.
+//
+
+#include <regex>
+#include <iostream>
+
+int main(){
+    using namespace std;
+    string data = "<person><first>Nico</first><last>Josuttis</last></person>";
+    cout << "data: " << data << '\n';
+
+    regex reg{R"(<(.*)>([^>]*)</(\1)>)"};  //.*表示不包括换行符的任意字符。
+
+    sregex_iterator pos{data.cbegin(), data.cend(), reg};
+    sregex_iterator end;
+
+    for (;  pos!=end; ++pos) {
+        cout << "match: " << pos->str() << '\n';
+        cout << "tag: " << pos->str(1) << '\n';
+        cout << "value: " << pos->str(2) << '\n';
+    }
+
+    sregex_iterator beg{data.cbegin(), data.cend(), reg};
+    for_each(beg, end, [](const smatch& m){
+        cout << "match: " << m.str() << '\n';
+        cout << "tag: " << m.str(1) << '\n';
+        cout << "value: " << m.str(2) << '\n';
+        cout << "position:" << m.position() << '\n';
+    });
+
+}
