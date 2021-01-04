0326e6dac2cea1d7d775084e3bc34e682d10d581
wq <seeman1978@163.com>
2021-01-04 22:27:10 +0800

    c++ 20 regex_search smatch

---
+++ regex/regex5.cpp
@@ -0,0 +1,23 @@
+//
+// Created by wq on 2021/1/4.
+//
+
+#include <regex>
+#include <iostream>
+
+int main(){
+    using namespace std;
+    string data = "<person><first>Nico</first><last>Josuttis</last></person>";
+    cout << "data: " << data << '\n';
+    smatch m;
+    regex reg{R"(<(.*)>([^>]*)</(\1)>)"};  //.*表示不包括换行符的任意字符。
+    auto pos = data.cbegin();
+    auto end = data.cend();
+    for (;  regex_search(pos, end, m, reg) ; pos=m.suffix().first) {
+        cout << "match: " << m.str() << '\n';
+        cout << "tag: " << m.str(1) << '\n';
+        cout << "value: " << m.str(2) << '\n';
+    }
+
+
+}
