786c8c35b6085aa1d5b48d10ee781d2ef358f8d9
wangqiang <seeman1978@163.com>
2021-01-20 11:43:57 +0800

    c++标准学习 利用cv实现生产者和消费者

--- locale/widen.cpp
+++ locale/widen.cpp
@@ -18,8 +18,16 @@ void try_widen2(const std::ctype<wchar_t>& f, char* c)
     wchar_t str2 [16];
     bool result = f.widen(c, c+strlen(c), &str2[0]) != 0;
     str2[strlen(c)] = '\0';
-    std::cout << "The single-byte character " << +(unsigned char)*c
-              << " widens to " << +str2 << '\n';
+    std::cout << "The single-byte character ";
+    for (int i=0; i<strlen(c); ++i){
+        std::cout << +(unsigned char)*(c+i);
+    }
+
+    std::cout << " widens to ";
+    for (int i=0; i<strlen(c); ++i){
+        std::cout << +(str2[i]);
+    }
+    std::cout << '\n';
 }
 
 int main()
@@ -37,15 +45,15 @@ int main()
         std::locale::global(std::locale("cs_CZ.utf-8"));    //macos
         auto& f2 = std::use_facet<std::ctype<wchar_t>>(std::locale());
         std::cout << "In Czech UTF-8 locale:\n";
-        try_widen(f2, 'a');
+        try_widen2(f2, "a");
 
-        try_widen2(f2, "\xC3\xF9");
-        try_widen(f2, '\xec');
+        try_widen2(f2, "\xF9\xC3");
+        try_widen2(f2, "\xec");
     }
 
     {
         using namespace std;
-        locale loc1 ( "English" );
+        locale loc1 ("en_US.UTF-8");
         char *str1 = "Hello everyone!";
         wchar_t str2 [16];
         bool result1 = (use_facet<ctype<wchar_t> > ( loc1 ).widen
