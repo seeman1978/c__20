208a922c37330e79930dedf66fa40713167d6a5a
wangqiang <seeman1978@163.com>
2021-01-23 18:11:37 +0800

    c++标准学习 atomic store load

--- Parallel/atomic.cpp
+++ Parallel/atomic.cpp
@@ -2,3 +2,31 @@
 // Created by 王强 on 2020/11/10.
 //
 
+#include <iostream>
+#include <atomic>
+#include <thread>
+#include <future>
+
+long data;
+std::atomic<bool> readyFlag{false};
+
+void provider(){
+    std::cout << "Please input a character: " << '\n';
+    std::cin.get();
+    data = 42;
+
+    readyFlag.store(true);
+}
+
+void consumer(){
+    while (!readyFlag.load()){
+        std::cout.put('.').flush();
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+    std::cout << "\nvalue: " << data << '\n';
+}
+
+int main(){
+    auto p = std::async(std::launch::async, provider);
+    auto c = std::async(std::launch::async, consumer);
+}
