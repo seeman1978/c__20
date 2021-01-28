d40b11488a7706d60631739c9d55624d89d98c11
wangqiang <seeman1978@163.com>
2021-01-28 18:28:12 +0800

    c++标准学习 给了一个 double check lock 运行时会发生不确定行为的例子。

--- Parallel/double_checked_lock.cpp
+++ Parallel/double_checked_lock.cpp
@@ -17,14 +17,27 @@ X* pX= nullptr;
 std::mutex lx;
 std::atomic<bool> x_init{false};
 
+//new操作会被编译器分为三步：1、分配内存空间；2、调用构造函数，初始化；3将分配的地址赋值给指针。
+//这三个步骤，在不同的编译器中会不相同，有个编译器会颠倒步骤2和3。如果步骤2和3颠倒，则下面这段
+//代码在有时将产生不可预期的行为。
+/*
+X* getInstance(){
+    if (pX == nullptr){
+        std::lock_guard<std::mutex> lg(lx);
+        if (px == nullptr){
+            pX = new X(8);
+        }
+    }
+    return pX;
+}*/
+
 X* getInstance(){
     if (!x_init){
-        lx.lock();
+        std::lock_guard<std::mutex> lg(lx);
         if (!x_init){
             pX = new X(8);
             x_init = true;
         }
-        lx.unlock();
     }
     return pX;
 }
