4266e9abb2458e747613161c2922861d98d39ef8
wq <seeman1978@163.com>
2021-02-16 23:22:49 +0800

    c++ 20 转义字符

--- escape_character.cpp
+++ escape_character.cpp
@@ -5,9 +5,10 @@
 #include <iostream>
 
 int main(){
+    //注意，如果反斜线\后面跟着的八进制数字超过3个，只有前3个数字与\构成转义序列。
+    std::cout << "\1151\n"; //输出M1
 
-    std::cout << "\1151\n";
-
+    //相反，\x要用到后面跟着的所有数字，表示一个字符，例如，"\x1234"表示一个16位的字符，
     std::wcout.imbue(std::locale("sv_SE.UTF-8"));
     std::wcout << L"\x00e5" << std::endl;
     std::wcout << L"\x00e51" << std::endl;
