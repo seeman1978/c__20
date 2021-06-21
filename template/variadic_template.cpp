20b8c9aa0985b9d48061b262880499ba08fcca2d
wq <seeman1978@163.com>
2021-06-22 06:54:10 +0800

    如何使用变长模版，实现任何数值类型的乘法？ 这个版本尚未实现

--- template/variadic_template.cpp
+++ template/variadic_template.cpp
@@ -1,7 +1,7 @@
 //
 // Created by 王强 on 2021/6/21.
 // 变长模版 ： 由模版参数包和函数参数包，两部分组成
-
+#include <iostream>
 template <typename ... Elements>
 class MyTuple;
 template<typename Head, typename ... Tail>
@@ -9,3 +9,36 @@ class MyTuple<Head, Tail... >:private MyTuple<Tail... >{
     Head head;
 };
 template<> class MyTuple<>{};
+
+// 非类型模版
+template<long... nums>
+struct Multiply;
+
+template<long first, long... last>
+struct Multiply<first, last...>{
+    static const long val = first * Multiply<last... >::val;
+};
+
+template<>
+struct Multiply<>{
+    static const long val = 1;
+};
+
+// 类型模版
+template<typename ... nums>
+struct MultiplyT;
+
+template<typename first, typename ... last>
+struct MultiplyT<first, last...>{
+    static const first val = MultiplyT<first>::value * MultiplyT<last... >::val;
+};
+
+template<>
+struct MultiplyT{
+    static const long val = 1;
+};
+
+int main(){
+    std::cout << Multiply<2, 3, 4, 5>::val << '\n';
+    std::cout << MultiplyT<float, 22, 44, 66, 88, 9>::val << '\n';
+}
