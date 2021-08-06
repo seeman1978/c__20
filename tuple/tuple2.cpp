3eb4b15f227413361029f07d86cee7c282f4020a
qiang wang <qiang.wang@megatronix.co>
2021-08-06 10:16:19 +0800

    forward_as_tuple

--- tuple/tuple2.cpp
+++ tuple/tuple2.cpp
@@ -1,5 +1,6 @@
 #include <tuple>
 #include <iostream>
+#include <thread>
 
 //
 // Created by qiangwang on 2021/8/5.
@@ -23,14 +24,33 @@ void foo2(std::tuple<T...> TupleTest){
     grade2 = 100.5;
 }
 
+template <typename... T>
+void foo3(std::tuple<T...> TupleTest){
+    auto [ gpa2, name2, grade2  ] = TupleTest;
+
+    std::cout << "gpa2 is " << gpa2 << '\n';
+    std::cout << "name is " << name2 << '\n';
+    std::cout << "grade2 is " << grade2 << '\n';
+
+    gpa2 = 10;
+    name2 = "qiang";
+    grade2 = 100.5;
+}
+
 int main(){
     // make_tuple so we don't need to enter all the type names
-    foo(0, std::make_tuple(2, "TEST", 5.5));
+//    foo(0, std::make_tuple(2, "TEST", 5.5));
 
     int i = 9;
     std::string name="wangqiang";
     double score = 98.5;
-    foo2(std::forward_as_tuple(i, name, score));
+//    foo2(std::forward_as_tuple(i, name, score));
+//    std::cout << "gpa2 is " << i << '\n';
+//    std::cout << "name is " << name << '\n';
+//    std::cout << "grade2 is " << score << '\n';
+
+    auto t = std::thread(foo3<int&, std::string, double>, std::forward_as_tuple(i, name, score));
+    t.join();
     std::cout << "gpa2 is " << i << '\n';
     std::cout << "name is " << name << '\n';
     std::cout << "grade2 is " << score << '\n';
