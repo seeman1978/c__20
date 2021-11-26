74ace5c01f69c78def96bed46694f1dbc4f877aa
qiang wang <qiang.wang@megatronix.co>
2021-11-26 19:42:16 +0800

    tuple forword_as_tuple

--- tuple/tuple3.cpp
+++ tuple/tuple3.cpp
@@ -22,6 +22,22 @@ void handle2(std::tuple<T...> tupleParam){
     v1 = 99;
 }
 
+template <typename... T>
+void handle3(std::tuple<T...> tupleParam){
+    auto v1 = std::get<0>(tupleParam);
+    std::cout << "i is " << v1 << '\n';
+    v1 = 33;
+    std::get<0>(tupleParam) = 44;
+}
+
+template <typename... T>
+void handle4(std::tuple<T...> tupleParam){
+    int v1;
+    std::tie(v1) = tupleParam;
+    std::cout << "i is " << v1 << '\n';
+    v1 = 33;
+}
+
 int main(){
     int i = 9;
     handle(std::tuple_cat(std::make_tuple(1, "hellow"), std::forward_as_tuple(i)));
@@ -29,5 +45,11 @@ int main(){
 
     st st1{"hello world", 100};
     handle2(std::forward_as_tuple(st1.i));
+    std::cout << "st i is " << st1.i << '\n';
+
+    handle3(std::forward_as_tuple(st1.i));
+    std::cout << "st i is " << st1.i << '\n';
+
+    handle4(std::forward_as_tuple(st1.i));
     std::cout << "st i is " << st1.i << '\n';
 }
