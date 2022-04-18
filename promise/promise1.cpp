81a9ce15098ca87652be6a8544fcac982512cbdf
qiang wang <qiang.wang@megatronix.co>
2022-04-18 20:03:48 +0800

    vector emplace_back initialize_list only for c++20

--- promise/promise1.cpp
+++ promise/promise1.cpp
@@ -1,5 +1,8 @@
 #include <future>
-#include <map>
+#include <thread>
+#include <vector>
+#include <numeric>
+#include <iostream>
 
 /**
 * @file
@@ -9,26 +12,23 @@
 * @date		    2022/3/11
 * @par Copyright(c): 	2022 mega. All rights reserved.
 */
-void consume(){
 
-    p.set_value();
+void accumulate(std::promise<int> accumulate_promise)
+{
+    int sum = 9;
+    accumulate_promise.set_value(sum);  // Notify future
 }
 
+int main()
+{
+    // Demonstrate using promise<int> to transmit a result between threads.
+    std::promise<int> accumulate_promise;
+    std::future<int> accumulate_future = accumulate_promise.get_future();
+    std::thread work_thread(accumulate, std::move(accumulate_promise));// must move
 
-void produce(int i){
-    std::promise<std::string> p;
-
-    std::thread t{consume, p};
-
-
-
-}
-
-
-
-
-
-int main(){
-
-
+    // future::get() will wait until the future has a valid result and retrieves it.
+    // Calling wait() before get() is not needed
+    //accumulate_future.wait();  // wait for result
+    std::cout << "result=" << accumulate_future.get() << '\n';
+    work_thread.join();  // wait for thread completion
 }
